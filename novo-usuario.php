0a563075db317f508d9bcb0eb5a651ffbbe289ec
Leiliane Nascimento <121403224+leilianenascimento@users.noreply.github.com>
2023-12-04 19:08:38 -0300

    Update novo-usuario.php

--- novo-usuario.php
+++ novo-usuario.php
@@ -1,1 +1,16 @@
 <h1>Novo Usuario</h1>
+<form action="?page=salvar" method="POST">
+    <div class="mb-3">
+        <label>Nome</label>
+        <input type="text" name="nome" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>E-mail</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Senha</label>
+        <input type="password" name="senha" class="form-control">
+        <button type="submit" class="">Enviar</button>
+    </div>
+</form>
