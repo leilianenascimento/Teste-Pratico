78795040ed57f909697bdf14525cb740e1d079d5
Leiliane Nascimento <121403224+leilianenascimento@users.noreply.github.com>
2023-12-04 19:07:27 -0300

    Add files via upload

---
+++ salvar.php
@@ -0,0 +1,13 @@
+<?php 
+   switch($_REQUEST["acao"]){
+      case'cadastrar':
+         $nome=$_POST["nome"];
+         $email=$_POST["email"];
+         $senha=$_POST["senha"];
+         break;
+
+         case "editar":
+            break;
+            
+
+   }
