6ba57422672489b70f3bf09bb7f35d99b0914ad1
Leiliane Nascimento <121403224+leilianenascimento@users.noreply.github.com>
2023-12-04 18:34:55 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    define( 'HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS','');
+    define('cadastro','usuarios');
+
+
+    $conn = new mysqli(HOST,USER,PASS,cadastro);
+
+    ?>
